3ae238cb026afd4da4a66019f7813bf6d374080b
Lenovo <jjjadhavsssttt@gmail.com>
2021-01-09 19:40:34 +0530

    second commit

--- courses/models.py
+++ courses/models.py
@@ -33,7 +33,7 @@ class Module(models.Model):
     course = models.ForeignKey(Course, related_name="modules", on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     description = models.TextField(blank=True)
-    
+
 
     def __str__(self):
         return self.title
